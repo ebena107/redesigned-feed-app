3221a08c7a3e92f313c0ebb4c593ccb6922b5520
AJAYI OLAYIDE <ebena107@gmail.com>
2026-02-15 13:03:12 +0100

    Upgrade dependencies for Android 15 compliance
    - Update Material Design library to 1.12.0 for Android 15 support
    - Run flutter pub upgrade to get latest compatible versions
    - All deprecated edge-to-edge APIs now removed
    - Flutter 3.38.4 includes full Android 15 edge-to-edge support
    - Verified no regressions: flutter analyze shows no issues

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import devicelocale
 import file_picker
 import in_app_review
 import package_info_plus
-import path_provider_foundation
 import printing
 import share_plus
 import shared_preferences_foundation
@@ -22,7 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
